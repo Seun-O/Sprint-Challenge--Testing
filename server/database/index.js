a9c4c5e44a658f98fe09545a4249c9eda317429e
Seun-O <12373359+Seun-O@users.noreply.github.com>
2019-06-29 12:54:14 -0400

    Test environment set up

--- server/database/index.js
+++ server/database/index.js
@@ -1,21 +1,39 @@
 const knex = require("knex");
 
 const config = {
-  development: {
-    client: "postgresql",
-    connection: {
-      database: "challengedb"
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: "knex_migrations"
-    }
+  client: "postgresql",
+  connection: {
+    database: "challengedb"
+  },
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: "knex_migrations"
   }
 };
 const db = knex(config);
+
+async function find(id) {
+  if (id) {
+    return db("games")
+      .where({ id })
+      .first();
+  }
+  return db("games");
+}
+
+// const execute = async () => {
+//   try {
+//     const data = await find();
+//     console.log(data);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// };
+
+// execute();
 
 module.exports = {
   db
--- server/server.test.js
+++ server/server.test.js
@@ -1,2 +1,12 @@
-const server = require("server");
+const server = require("./server");
 const request = require("supertest");
+
+describe("GET /", () => {
+  it("sever should be up and running", done => {
+    request(server)
+      .get("/")
+      .set("Accept", "application/json")
+      .expect("Content-Type", /json/)
+      .expect(200, done);
+  });
+});
